e68082f6edfcba54d17c00afeba82cb63e5dc45f
Carol Wanzuu <carolkyalo01@gmail.com>
2022-03-02 12:15:35 +0300

    footer template

--- Students/views.py
+++ Students/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 
